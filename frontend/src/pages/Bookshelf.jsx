8d0e922c1653a4c0c8cf8916d32024384c8d66ec
Ray Forman <rayforman150@gmail.com>
2024-12-22 05:06:59 -0500

    Bookshelf edits, general cleanup

--- frontend/src/pages/Bookshelf.jsx
+++ frontend/src/pages/Bookshelf.jsx
@@ -29,205 +29,62 @@ const Bookshelf = () => {
 
   const books = [
     {
-      title: "Mindset: The New Psychology of Success",
-      author: "Carol S. Dweck",
-      description: "A transformative book on the power of mindset in achieving success.",
-      link: "https://www.amazon.com/Mindset-Psychology-Carol-S-Dweck/dp/0345472322",
-      image: "/mindset.png", // Updated image path
-      tags: ["Psychology", "Personal Development"]
+      title: "Greenlights",
+      author: "Matthew McConaughey",
+      description: "An unconventional memoir filled with raucous stories, outlaw wisdom, and lessons learned the hard way about living with greater satisfaction.",
+      link: "https://www.amazon.com/Greenlights-Matthew-McConaughey/dp/0593139135?crid=15BU12F9FEXR3&dib=eyJ2IjoiMSJ9.BWiy_Rf_Yec1rhqYEPECNT8pgzUXbtX2BZX7GjLMqy5BK9_8CDAv8gjXZhU9UEmDR1WsFlNe1fINtODdRgX-CAqQABOIJCA-0-XuYxlsGHFIlzxTJ0YL9eaL2o_mpkR-WMWKifZCMvqRThd1ppZ1aL-rJ9DcfYDjFVGoIXPm7E7wBHp0O59ooDMpKqHq8M6q7axMct8BSH6KtlE-4X6NEJMUaCqk1oMnJ2t_oM9b4Wc.ov04SPi8wjSZfOdpZ_1ybfpoYDrmhkuXbT0R82ljaNo&dib_tag=se&keywords=greenlights&qid=1734860352&sprefix=greenlight%2Caps%2C116&sr=8-1",
+      image: "/greenlights.png",
+      tags: ["Personal Development", "Gratitude", "Philosophy", "Self-Discovery"]
     },
     {
-      title: "How to Make Friends & Influence People",
+      title: "How to Win Friends & Influence People",
       author: "Dale Carnegie",
       description: "A timeless guide to building relationships and influencing others.",
       link: "https://www.amazon.com/How-Win-Friends-Influence-People/dp/0671027034",
-      image: "/how-to-win-friends.png", // Updated image path
-      tags: ["Communication", "Personal Development"]
+      image: "/how-to-win-friends.png",
+      tags: ["Personal Development", "Communication", "Relationships", "Leadership"]
     },
     {
-      title: "Small Journey into the World of Quanta (French)",
-      author: "Etienne Klein",
-      description: "An exploration of the quantum world through the lens of physics.",
-      link: "https://www.amazon.fr/Petit-voyage-dans-monde-quanta/dp/2081227010",
-      image: "/petit-voyage.png", // Updated image path
-      tags: ["Philosophy", "Science", "Physics"]
+      title: "King, Warrior, Magician, Lover",
+      author: "Robert Moore & Douglas Gillette",
+      description: "A guide to positively harnessing the four archetypes of the mature masculine in the modern age.",
+      link: "https://www.amazon.com/King-Warrior-Magician-Lover-Rediscovering/dp/0062506064",
+      image: "/king-warrior.png",
+      tags: ["Personal Development", "Healthy Masculinity", "Introspection", "Self Improvement"]
+    },
+    {
+      title: "The Four Agreements",
+      author: "Don Miguel Ruiz",
+      description: "A practical guide to personal freedom that reveals the source of self-limiting beliefs.",
+      link: "https://www.amazon.com/Four-Agreements-Practical-Personal-Freedom/dp/1878424319",
+      image: "/four_agreements.png",
+      tags: ["Personal Development", "Inner-Peace", "Introspection"]
+    },
+    {
+      title: "Hidden Potential",
+      author: "Adam Grantz",
+      description: "A science-based approach to achieving greater success and unlocking your full potential.",
+      link: "https://www.amazon.com/Hidden-Potential-Science-Achieving-Greater/dp/0593653149?_encoding=UTF8&dib_tag=se&dib=eyJ2IjoiMSJ9.VNA70kI6SrWtjZj6ammdpf_7HRoEHdy8bRkKOa02j9kU8d6P7oVc7sR6E23qteY8iHAoc5lNmCv4KP71xfbweyf54_nMbEd4GmtWyMdTa3SN98XE_gpP0enokWHBcvkuePBg-oXn_ovVhuDbv53M5EdZM6bFUMQqfgDZF0cDpJmQWSux3ZjrK73IdoGs3ovIj-lO-d5jcMXvGCkhrnno2Z4sQCI4u_l-1NwQQ0xCtxE.IhMUa_m-bd3FevSY5mB2CfsW9Y1xtDqHeJi8PNaqnO8&qid=1734860971&sr=1-1",
+      image: "/hidden-potential.png",
+      tags: ["Personal Development", "Self Realization"]
     },
     {
       title: "Meditations",
-      author: "Ray Descartes",
-      description: "Philosophical meditations on the nature of existence and reality.",
-      link: "https://www.amazon.com/Descartes-Meditations-Philosophy-Selections-Objections/dp/0521558182",
-      image: "/meditations.png", // Updated image path
-      tags: ["Philosophy", "Classics"]
-    },
-    {
-      title: "The Spatial Web",
-      author: "Gabriel Ray & Dan Mapes",
-      description: "A visionary book on the future of the internet and digital spaces.",
-      link: "https://www.amazon.com/Spatial-Web-connect-machines-transform/dp/0578562960",
-      image: "/spatial-web.png", // Updated image path
-      tags: ["Technology", "Futurism","AI", "Web3.0"]
-    },
-    {
-      title: "The Psychology of Money: Timeless Lessons on Wealth, Greed, and Happiness",
-      author: "Morgan Housel",
-      description: "An insightful look into the psychology behind our financial decisions, revealing how money moves us in unexpected ways.",
-      link: "https://a.co/d/7WN3b6T",
-      image: "/psychology_of_money.jpg",
-      tags: ["Finance", "Psychology"]
-    },
-    {
-      title: "Neurosys",
-      author: "Dr. Michel F. Forman",
-      description: "A gripping thriller that dives into the depths of cyber warfare, global conspiracies, and a super-intelligent synthetic brain under attack.",
-      link: "https://a.co/d/fIt7eOS",
-      image: "/neurosys.jpg",
-      tags: ["Thriller", "Futurism","Technology"]
-    },
-    {
-      title: "Meditations (French)",
       author: "Marcus Aurelius",
-      description: "A timeless collection of personal reflections by the Roman Emperor, offering insights into leadership, resilience, and the pursuit of a virtuous life.",
-      link: "https://a.co/d/eyYX2MI",
+      description: "A series of personal writings by the Roman Emperor and philosopher king, providing a window into his philosophy on life.",
+      link: "https://www.amazon.com/Meditations-Marcus-Aurelius/dp/1503280462",
       image: "/meditations_aurelius.jpg",
-      tags: ["Philosophy", "Classics"]
+      tags: ["Personal Development", "Leadership", "Philosophy", "Stoicism", "Introspection"]
     },
     {
-      title: "Screw It, Let's Do It: Lessons in Life",
-      author: "Richard Branson",
-      description: "An inspiring guide from the maverick entrepreneur, sharing his lessons on business, adventure, and embracing challenges with a bold mindset.",
-      link: "https://a.co/d/54PHOtf",
-      image: "/screw_it_lets_do_it.jpg",
-      tags: ["Business", "Entrepreneurship"]
+      title: "Atomic Habits",
+      author: "James Clear",
+      description: "A simple guide to breaking bad habits and adopting good ones.",
+      link: "https://www.amazon.com/Atomic-Habits-Proven-Build-Break/dp/0735211299?crid=WEXZ9LZEBCIN&dib=eyJ2IjoiMSJ9.xM2BHrYsqyJHM0kyPlLqc-wG9fYUkRW_VvUzSYeB1rz9hcBsE2Qxj09KsdhiMPNZuuMMdlsFxF74l00dF3mNIEUmC5BpNGDx9u0ORiBra-_wXr459_oYa05l5UZt9gpPyVs2xm2wXmAuB85i9YQCEdIYN_JTeRkwNcfqgZaKmG5KvBVJOEkZsXPPNw9vqSvdHU3Ap03CPBUCKzj8v1Crr8uILxZzf_yHlyc0jIq0P08.JCM5oNtQLjBlqCqEyThehHG_TdS4MurL5lJsj7WAVrg&dib_tag=se&keywords=atomic+habits&qid=1734861177&s=books&sprefix=atomi%2Cstripbooks%2C106&sr=1-1",
+      image: "/atomic-habits.png",
+      tags: ["Personal Development", "Daily Improvement", "Lifestyle Design"]
     },
-    {
-      title: "The Little Prince (French)",
-      author: "Antoine de Saint-Exupéry",
-      description: "A beloved tale that blends whimsy and profound wisdom, exploring themes of friendship, love, and the essence of human nature.",
-      link: "https://a.co/d/hYVf0cu",
-      image: "/the_little_prince.jpg",
-      tags: ["Classics", "Philosophy", "French Literature"]
-    },
-    {
-      title: "The Princess de Clèves (French)",
-      author: "Madame de La Fayette",
-      description: "A classic French novel set in the royal court, exploring themes of love, honor, and the complexities of human emotions.",
-      link: "https://a.co/d/i2zlsUb",
-      image: "/princess_de_cleves.jpg",
-      tags: ["Classics", "French Literature", "Romance"]
-    },
-    {
-      title: "Murder on the Orient Express (French)",
-      author: "Agatha Christie",
-      description: "A classic whodunit mystery that unravels aboard a luxurious train, featuring the brilliant detective Hercule Poirot.",
-      link: "https://a.co/d/7KiT5G8",
-      image: "/murder_on_the_orient_express.jpg",
-      tags: ["Mystery", "Classics", "Crime"]
-    },
-    {
-      title: "Socratic Dialogues: Euthyphro, Apology, Crito, Meno, Phaedo (French)",
-      author: "Plato",
-      description: "A collection of dialogues depicting Socrates' defense of his philosophy, his ethical principles, and his final days.",
-      link: "https://a.co/d/bYnWLFj",
-      image: "/socrates_apology.jpg",
-      tags: ["Philosophy", "Classics", "Ethics"]
-    },
-    {
-      title: "Essays (French)",
-      author: "Michel de Montaigne",
-      description: "An influential collection of personal reflections on human nature, society, and the self, pioneering the essay as a literary form.",
-      link: "https://a.co/d/9PYTUQW",
-      image: "/essays_montaigne.jpg",
-      tags: ["Philosophy", "Classics", "French Literature"]
-    },
-    {
-      title: "Les Contemplations (French)",
-      author: "Victor Hugo",
-      description: "A deeply personal collection of poetry reflecting on themes of love, loss, and the human condition, capturing Hugo's profound vision.",
-      link: "https://a.co/d/0YO8mTF",
-      image: "/les_contemplations.jpg",
-      tags: ["Poetry", "Classics", "French Literature"]
-    },
-    {
-      title: "Phaedra (French)",
-      author: "Jean Racine",
-      description: "A powerful French tragedy exploring the destructive power of forbidden love and the complexities of human passion.",
-      link: "https://a.co/d/94ERWso",
-      image: "/phedre.jpg",
-      tags: ["Drama", "Classics", "French Literature"]
-    },
-    {
-      title: "Antigone (French)",
-      author: "Sophocles",
-      description: "A timeless Greek tragedy that examines the conflict between personal duty and the laws of the state, questioning moral choices.",
-      link: "https://a.co/d/cWJAMWV",
-      image: "/antigone.jpg",
-      tags: ["Drama", "Classics", "Greek Literature"]
-    },
-    {
-      title: "Incendies (French)",
-      author: "Wajdi Mouawad",
-      description: "A heart-wrenching play that journeys through war, identity, and the unbreakable bonds of family, unearthing hidden pasts.",
-      link: "https://a.co/d/0ykTnt6",
-      image: "/incendies.jpg",
-      tags: ["Drama", "Modern Literature", "War"]
-    },
-    {
-      title: "Pierre and Jean (French)",
-      author: "Guy de Maupassant",
-      description: "A compelling novella exploring themes of jealousy, family dynamics, and the complexity of human emotions.",
-      link: "https://a.co/d/5eoV0Nf",
-      image: "/pierre_and_jean.jpg",
-      tags: ["Classics", "French Literature", "Novel"]
-    },
-    {
-      title: "A Woman’s Life (French)",
-      author: "Guy de Maupassant",
-      description: "A poignant story of a young woman's struggles, capturing the disillusionment and hardships of life in 19th-century France.",
-      link: "https://a.co/d/c30YWOV",
-      image: "/une_vie.jpg",
-      tags: ["Classics", "French Literature", "Novel"]
-    },
-    {
-      title: "L'Ingénu (French)",
-      author: "Voltaire",
-      description: "A satirical tale that critiques society, religion, and human folly through the eyes of a naive young man discovering civilization.",
-      link: "https://a.co/d/cmkyX3S",
-      image: "/lingenu.jpg",
-      tags: ["Philosophy", "Satire", "French Literature"]
-    },
-    {
-      title: "Intruder (French)",
-      author: "Maurice Maeterlinck",
-      description: "A haunting play that delves into the unseen forces of fate, fear, and the supernatural, shrouded in an atmosphere of suspense.",
-      link: "https://a.co/d/9me2e7s",
-      image: "/intruder.jpg",
-      tags: ["Drama", "Symbolism", "Belgian Literature"]
-    },
-    {
-      title: "Happy Days (French)",
-      author: "Samuel Beckett",
-      description: "An absurdist play that captures the human condition, resilience, and existential despair through Beckett’s unique minimalist style.",
-      link: "https://a.co/d/9tkVrru",
-      image: "/happy_days.jpg",
-      tags: ["Drama", "Absurdism", "Modern Literature"]
-    },
-    {
-      title: "Marx et la poupée (French)",
-      author: "Maryam Madjidi",
-      description: "A poignant memoir blending personal history with cultural identity, capturing the author's journey from Iran to France and the clash of two worlds.",
-      link: "https://a.co/d/gaZxOCd",
-      image: "/marx_et_la_poupee.jpg",
-      tags: ["Memoir", "Cultural Identity", "French Literature"]
-    },
-    {
-      title: "AI 2041: Ten Visions for Our Future",
-      author: "Kai-Fu Lee & Chen Qiufan",
-      description: "A visionary exploration of how AI will shape the next two decades, blending sci-fi storytelling with insightful analysis of technological advancements.",
-      link: "https://a.co/d/4Lhn9c6",
-      image: "/ai_2041.jpg",
-      tags: ["Technology", "Futurism", "AI"]
-    },
+
     // Add more books as needed
   ];
 
