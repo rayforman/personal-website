deae87fcc2aa4c8f9fc48ecf871cfed4d034c7fa
Ray Forman <rayforman150@gmail.com>
2025-01-13 16:00:45 -0500

    Animation descriptions pt. 1

--- frontend/src/pages/Animation.jsx
+++ frontend/src/pages/Animation.jsx
@@ -65,49 +65,49 @@ const Animation = () => {
       title: "Ratatouille",
       studio: "Pixar",
       image: "/ratatouille.jpg",
-      description: "Your film description here..."
+      description: "My favorite movie of all time, animated or otherwise. This movie speaks to me on a level so personal I reserve watching it for momentous occasions. As someone who used to cook professionally and now as a hobby, this movie holds a special place in my heart. This visually stunning, musically rich, and emotionally resonant film has, more than any other artwork, inspired me to chase my dreams. Because after all, anyone can cook."
     },
     {
       rank: 2,
-      title: "Kung Fu Panda",
-      studio: "Dreamworks",
-      image: "/kungfupanda.png",
-      description: "Your film description here..."
-    },
-    {
-      rank: 3,
-      title: "How to Train Your Dragon",
-      studio: "Dreamworks",
-      image: "/httyd.png",
-      description: "Your film description here..."
-    },
-    {
-      rank: 4,
-      title: "The Lion King",
-      studio: "Disney",
-      image: "/lionking.png",
-      description: "Your film description here..."
-    },
-    {
-      rank: 5,
-      title: "Coco",
-      studio: "Pixar",
-      image: "/coco.png",
-      description: "Your film description here..."
-    },
-    {
-      rank: 6,
       title: "Spider-Man: Into the Spider-Verse",
       studio: "Sony Pictures Animation",
       image: "/spiderverse.png",
-      description: "Your film description here..."
+      description: "The bold 2018 film that redefined high-budget animation and inspired studios around the world to explore stylized animation at scale. The most visually adventurous feature film I'd scene until its sequel in 2023 which further pushed the envelope with a whole array of additional animation styles to represent various multiverses. This film, better than any other, puts on display why animation is artistically unique and boundless, and has since inspired so many artists to do the same."
+    },
+    {
+      rank: 3,
+      title: "Kung Fu Panda",
+      studio: "Dreamworks",
+      image: "/kungfupanda.png",
+      description: "This movie is called Kung Fu Panda, likely the most ridiculous name on this list. Yet it manages to be an emotionally and philosophically heavy masterpiece that explores the challenges of identity, prejudice, and becoming who one's meant to be. The point? "
+    },
+    {
+      rank: 4,
+      title: "How to Train Your Dragon",
+      studio: "Dreamworks",
+      image: "/httyd.png",
+      description: "Another silly name that you'd never expect to be hiding a great story. This is a beautiful film about a son who goes against his father's expectations to be who he truly is. The father-son dynamic in this one is deeply personal to me and brings tears to my eyes every time. Also, John Powell hits it out of the park with some of the best musical motifs I've heard to date."
+    },
+    {
+      rank: 5,
+      title: "The Lion King",
+      studio: "Disney",
+      image: "/lionking.png",
+      description: "An incredibly famous movie that gets plenty of hype that I always skeptical of. After rewatching it, I have no doubt in my mind that this is one of the best movies ever made. From the intro sequence, to the beautiful musical themes, to the full-circle ending, this movie deserves every ounce of love it gets from the media."
+    },
+    {
+      rank: 6,
+      title: "Coco",
+      studio: "Pixar",
+      image: "/coco.png",
+      description: "A visual masterpiece that exhibits color in a way I've never seen before. Music is central to Coco as well as the Mexican culture it explores. The characters in this movie are well developed, the plot is largely unpredictable, and each payoff feels earned. This movie is what you get when good writing, meets great music and excellent animation."
     },
     {
       rank: 7,
       title: "Soul",
       studio: "Pixar",
       image: "/soul.png",
-      description: "Your film description here..."
+      description: "Soul is a very unique movie that takes on a bold goal: to tackle the meaning of life. This is an imperfect movie that at times can be too goofy for me, but at other times can connect with me at a spiritual level. What is it to be human? The scenes where 'Epiphany' plays on the score and when Joe jumps with 22 to Earth choke me up in a way I can't explain."
     },
     {
       rank: 8,
@@ -139,10 +139,10 @@ const Animation = () => {
         <div className="mb-12">
           <h1 className="text-4xl font-bold mb-4">Animation</h1>
           <p className="text-lg text-gray-300 mb-6">
-            My favorite artisitic medium. I grew up believing that animated movies were for kids. But as an adult, I've grown to appreciate animation for what it is: artwork in motion. If a painting is a photograph, an animation is a movie. And like paintings, the limit of an animation is the imagination. 
+            My favorite artisitic medium. I grew up believing that animated movies were for kids. But as an adult, I've grown to appreciate animation for what it is: artwork in motion. If a painting is a photograph, an animation is a movie. And like paintings, the limit of an animation is only our imagination. 
           </p>
           <p className="text-lg text-gray-300 mb-6">
-            For me, no art form can evoke more emotion than animation. Each frame is curated so carefully as to create the precise reaction desired in the viewer. In an excellent animated film, any frame chosen randomly could be a piece of art on its own. In conjunction with strong voice acting and an immersive soundtrack, what you're left with is an experience in a league of its own. 
+            For me, no art form can evoke more emotion than animation. Each frame is curated so carefully as to create the precise reaction desired in the viewer. In an excellent animated film, any frame chosen randomly could be a piece of art on its own. In conjunction with good writing, strong voice acting, and an immersive soundtrack, what you're left with is an experience in a league of its own. 
           </p>
         </div>
         {posts.length === 0 ? (
@@ -155,7 +155,7 @@ const Animation = () => {
         <p className="text-lg text-gray-300 mb-4">&nbsp;</p>
         <h1 className="text-4xl font-bold mb-4">My List</h1>
         <p className="text-lg text-gray-300 mb-6">
-          My lifelong dream is to make animation short films that immortalize my life philosophy and inspire people to live their best life. Until then, here is my personal ordered list of favorite animated films, limited to one per series. 
+          My lifelong dream is to make animated films that immortalize my life philosophy and inspire people to live their best life. Until then, here is an ordered list of my personal favorite animated films, limited to one per series:
         </p>
         <div className="space-y-8">
           {films.map((film) => (
